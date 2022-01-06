210c764ac800bbc48e1ca5d37ba4a6c7bf7de187
ImtiazHossainShovon <90201587+ImtiazHossainShovon@users.noreply.github.com>
2022-01-06 22:55:30 +0600

    Create MyFancyJavaProject.java

---
+++ MyFancyJavaProject.java
@@ -0,0 +1,6 @@
+public class CsharpCorner {  
+    public static void main(String[] args) {  
+        CsharpCorner c1 = new CsharpCorner();  
+        CsharpCorner c2 = new CsharpCorner();  
+    }  
+}
